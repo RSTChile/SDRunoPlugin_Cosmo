8d1cec211a8e2ada73f3fa82944e158f5e3252c1
Alexis López Tapia <103556334+RSTChile@users.noreply.github.com>
2025-09-21 03:37:47 -0300

    Refactor SDRunoPlugin_TemplateSettingsDialog
    Updated the info label to include a multiline message.

--- SDRunoPlugin_TemplateSettingsDialog.cpp
+++ SDRunoPlugin_TemplateSettingsDialog.cpp
@@ -6,7 +6,6 @@
 #include "SDRunoPlugin_TemplateSettingsDialog.h"
 #include "SDRunoPlugin_TemplateUi.h"
 
-// Constructor con UI padre y owner form
 SDRunoPlugin_TemplateSettingsDialog::SDRunoPlugin_TemplateSettingsDialog(SDRunoPlugin_TemplateUi& parent, nana::form& owner_form)
     : nana::form(owner_form, nana::size(dialogFormWidth, dialogFormHeight), nana::appearance(true, false, true, false, false, false, false))
     , m_parent(&parent)
@@ -16,26 +15,26 @@ SDRunoPlugin_TemplateSettingsDialog::SDRunoPlugin_TemplateSettingsDialog(SDRunoP
 
 SDRunoPlugin_TemplateSettingsDialog::~SDRunoPlugin_TemplateSettingsDialog()
 {
-    // Nana limpia sus manejadores al destruir controles
 }
 
 void SDRunoPlugin_TemplateSettingsDialog::Setup()
 {
-    // Centrar por defecto: no se leen posiciones desde el host (evita llamadas cross-thread)
+    // Center by default: no host calls from GUI thread
     move(nana::API::make_center(dialogFormWidth, dialogFormHeight));
     size(nana::size(dialogFormWidth, dialogFormHeight));
     caption("SDRuno Plugin Cosmo - Settings");
 
-    // Fondo estilo oscuro
+    // Dark background
     this->bgcolor(nana::color(45, 45, 48));
 
-    // Controles
+    // Controls
     titleLbl.caption("Cosmo Plugin Configuration");
     titleLbl.fgcolor(nana::color(220, 220, 220));
     titleLbl.transparent(true);
     titleLbl.text_align(nana::align::center, nana::align_v::center);
 
-    infoLbl.caption("Settings and configuration options will be available here.");
+    // Texto multilínea y auto-break
+    infoLbl.caption("Settings and configuration options will be available here.\n(More options coming soon)");
     infoLbl.fgcolor(nana::color(180, 180, 180));
     infoLbl.transparent(true);
 
@@ -45,15 +44,10 @@ void SDRunoPlugin_TemplateSettingsDialog::Setup()
 
     closeBtn.events().click([this]() {
         close();
-        if (m_parent) {
-            m_parent->SettingsDialogClosed();
-        }
+        if (m_parent) { m_parent->SettingsDialogClosed(); }
     });
 
-    // Avisar al padre cuando se cierra la ventana (por X o Alt+F4)
     events().unload([this](const nana::arg_unload&) {
-        if (m_parent) {
-            m_parent->SettingsDialogClosed();
-        }
+        if (m_parent) { m_parent->SettingsDialogClosed(); }
     });
 }
