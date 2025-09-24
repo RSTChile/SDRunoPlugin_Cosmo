9981a231485b97012660ce4a9f76b7a544aaaa7d
Alexis López Tapia <103556334+RSTChile@users.noreply.github.com>
2025-09-24 04:03:04 -0300

    Refactor stream observer registration and RMS calculation

--- SDRunoPlugin_Template.cpp
+++ SDRunoPlugin_Template.cpp
@@ -4,19 +4,19 @@
 #include <vector>
 #include <chrono>
 #include <cmath>
-
 #include "SDRunoPlugin_Template.h"
 #include "SDRunoPlugin_TemplateUi.h"
 
 SDRunoPlugin_Template::SDRunoPlugin_Template(IUnoPluginController& controller)
     : IUnoPlugin(controller), m_form(*this, controller), m_worker(nullptr)
 {
-    controller.RegisterStreamObserver(this);
+    m_channel = 0; // O el canal que corresponda
+    controller.RegisterStreamObserver(m_channel, this);
 }
 
 SDRunoPlugin_Template::~SDRunoPlugin_Template()
 {
-    m_controller.UnregisterStreamObserver(this);
+    m_controller.UnregisterStreamObserver(m_channel, this);
 }
 
 void SDRunoPlugin_Template::HandleEvent(const UnoEvent& ev)
@@ -24,10 +24,12 @@ void SDRunoPlugin_Template::HandleEvent(const UnoEvent& ev)
     m_form.HandleEvent(ev);
 }
 
-void SDRunoPlugin_Template::StreamUpdate(float* samples, int length)
+void SDRunoPlugin_Template::StreamObserverProcess(channel_t channel, const Complex* data, int length)
 {
+    // Calcula RMS de la señal
     float sum = 0.0f;
-    for (int i = 0; i < length; ++i) sum += samples[i] * samples[i];
+    for (int i = 0; i < length; ++i)
+        sum += std::norm(data[i]);
     float rms = sqrt(sum / length);
     m_signalPresent = (rms > 0.01f);
 
