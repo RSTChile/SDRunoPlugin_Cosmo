32f3f69edd4b5c87296c10c5a35dfb9a4a225956
Alexis López Tapia <103556334+RSTChile@users.noreply.github.com>
2025-09-24 05:48:23 -0300

    Actualizar SDRunoPlugin_TemplateForm.cpp

--- SDRunoPlugin_TemplateForm.cpp
+++ SDRunoPlugin_TemplateForm.cpp
@@ -7,8 +7,6 @@
 #include "SDRunoPlugin_TemplateSettingsDialog.h"
 #include "SDRunoPlugin_TemplateUi.h"
 #include "resource.h"
-#include <io.h>
-#include <shlobj.h>
 
 #define VERSION "V1.1"
 
@@ -32,13 +30,11 @@ void SDRunoPlugin_TemplateForm::Run()
     nana::exec();
 }
 
-// Inicializa todos los controles del formulario.
 void SDRunoPlugin_TemplateForm::Setup()
 {
     using namespace nana;
 
-    // Cargar imágenes desde la carpeta "resources". Si prefieres incrustarlas
-    // en el RC, usa FindResource/LoadImage como en el plugin DRM [oai_citation:2‡raw.githubusercontent.com](https://raw.githubusercontent.com/JvanKatwijk/SDRunoPlugin_drm/master/SDRunoPlugin_drmForm.cpp).
+    // Cargar imágenes desde la carpeta "resources"
     img_bg_border.open("resources\\winback.bmp");
     img_bg_inner.open("resources\\sp_blue_back.bmp");
     img_header.open("resources\\HEADER.dib");
@@ -57,7 +53,7 @@ void SDRunoPlugin_TemplateForm::Setup()
     if (!img_close_normal.empty()) close_button.load(img_close_normal);
     if (!img_sett_normal.empty()) sett_button.load(img_sett_normal);
 
-    // Colocar el header y los botones en sus posiciones
+    // Posicionamiento de los elementos superiores
     header_bar.move(rectangle(sideBorderWidth, 0,
                               formWidth - 2 * sideBorderWidth,
                               topBarHeight));
@@ -65,64 +61,60 @@ void SDRunoPlugin_TemplateForm::Setup()
     min_button.move(rectangle(formWidth - sideBorderWidth - 40, 0, 20, topBarHeight));
     sett_button.move(rectangle(formWidth - sideBorderWidth - 80, 0, 40, topBarHeight));
 
-    // Configurar título y versión
+    // Configurar título y etiqueta de versión
     title_bar_label.caption("Cosmo");
     title_bar_label.move(rectangle(sideBorderWidth + 5, 0, 150, topBarHeight));
     title_bar_label.fgcolor(colors::white);
-    title_bar_label.align(nana::align::left, nana::align_v::center);
-
     versionLbl.caption(VERSION);
     versionLbl.bgcolor(colors::transparent);
     versionLbl.fgcolor(colors::white);
 
-    // Preparar LED (mantiene las rutas originales)
+    // Preparar LED
     ledOnImg.open("resources\\led_on.bmp");
     ledOffImg.open("resources\\led_off.bmp");
     ledPicture.load(ledOffImg, rectangle(0, 0, 20, 20));
     ledPicture.transparent(true);
 
-    // Permitir arrastrar la ventana haciendo clic en cualquier zona del formulario
-    form_drag_label.bgcolor(colors::transparent);
+    // Permitir arrastrar la ventana
     form_dragger.target(*this);
     form_dragger.trigger(form_drag_label);
 
-    // Eventos de botón Close
-    close_button.events().mouse_down([&](const arg_mouse&) {
+    // Eventos para el botón Close
+    close_button.events().mouse_down([&] {
         if (!img_close_down.empty()) close_button.load(img_close_down);
     });
-    close_button.events().mouse_up([&](const arg_mouse&) {
+    close_button.events().mouse_up([&] {
         if (!img_close_normal.empty()) close_button.load(img_close_normal);
     });
-    close_button.events().click([&](const arg_click&) {
-        // Notificar al controlador para descargar el plugin
-        m_controller.RequestUnload(&m_parent);
+    close_button.events().click([&] {
+        // Notificar al UI que la ventana ha sido cerrada
+        m_parent.FormClosed();
     });
 
-    // Eventos de botón Minimize
-    min_button.events().mouse_down([&](const arg_mouse&) {
+    // Eventos para el botón Minimize
+    min_button.events().mouse_down([&] {
         if (!img_min_down.empty()) min_button.load(img_min_down);
     });
-    min_button.events().mouse_up([&](const arg_mouse&) {
+    min_button.events().mouse_up([&] {
         if (!img_min_normal.empty()) min_button.load(img_min_normal);
     });
-    min_button.events().click([&](const arg_click&) {
-        // Minimizar la ventana
-        this->minimize(true);
+    min_button.events().click([&] {
+        // Minimizar la ventana utilizando la API de Nana
+        nana::API::zoom_window(this->handle(), false);
     });
 
-    // Eventos de botón Settings
-    sett_button.events().mouse_down([&](const arg_mouse&) {
+    // Eventos para el botón Settings
+    sett_button.events().mouse_down([&] {
         if (!img_sett_down.empty()) sett_button.load(img_sett_down);
     });
-    sett_button.events().mouse_up([&](const arg_mouse&) {
+    sett_button.events().mouse_up([&] {
         if (!img_sett_normal.empty()) sett_button.load(img_sett_normal);
     });
-    sett_button.events().click([&](const arg_click&) {
+    sett_button.events().click([&] {
         SettingsButton_Click();
     });
 }
 
-// Cambia el estado del LED (encendido/apagado).
 void SDRunoPlugin_TemplateForm::SetLedState(bool on)
 {
     if (on) {
@@ -132,19 +124,19 @@ void SDRunoPlugin_TemplateForm::SetLedState(bool on)
     }
 }
 
-// Evento del botón de configuración: abre el diálogo de Settings.
 void SDRunoPlugin_TemplateForm::SettingsButton_Click()
 {
-    // Crear el diálogo de configuración y desactivar la ventana principal mientras está abierto
+    // Crear y mostrar el diálogo de configuración
     auto settingsDialog = new SDRunoPlugin_TemplateSettingsDialog(m_parent, *this, m_controller);
     this->enabled(false);
     settingsDialog->show();
-    // Puedes conectar el cierre del diálogo para volver a habilitar la ventana:
-    // settingsDialog->events().destroy([&]{ SettingsDialog_Closed(); });
+    // Cuando se cierre el diálogo, se debe volver a habilitar la ventana
+    // Puedes conectar events().destroy en settingsDialog para llamar a SettingsDialog_Closed
 }
 
 void SDRunoPlugin_TemplateForm::SettingsDialog_Closed()
 {
+    // Rehabilitar la ventana al cerrar el diálogo
     this->enabled(true);
     this->focus();
 }
