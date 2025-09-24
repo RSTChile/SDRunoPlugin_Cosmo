517181706bb8dfdcde8af2a20f9fdd0b589b62e3
Alexis López Tapia <103556334+RSTChile@users.noreply.github.com>
2025-09-24 05:48:52 -0300

    Actualizar SDRunoPlugin_TemplateUi.h

--- SDRunoPlugin_TemplateUi.h
+++ SDRunoPlugin_TemplateUi.h
@@ -5,15 +5,17 @@
 #include <nana/gui/widgets/label.hpp>
 #include <nana/gui/timer.hpp>
 #include <iunoplugin.h>
-#include <iostream>
-#include <iomanip>
-#include <sstream>
-
 #include <iunoplugincontroller.h>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <string>
+
 #include "SDRunoPlugin_TemplateForm.h"
 
 class SDRunoPlugin_Template;
 
+// Gestor de la interfaz del plugin (crea y controla el formulario)
 class SDRunoPlugin_TemplateUi
 {
 public:
@@ -30,14 +32,20 @@ public:
 
     void UpdateLed(bool signalPresent);
 
+    // *** Métodos añadidos para el diálogo de configuración ***
+    std::string GetBaseDir() const;
+    void RequestChangeBaseDir(const std::string& path);
+    void RequestChangeVrx(int vrxIndex);
+    void SettingsDialogClosed();
+
 private:
-    SDRunoPlugin_Template & m_parent;
+    SDRunoPlugin_Template& m_parent;
     std::thread m_thread;
     std::shared_ptr<SDRunoPlugin_TemplateForm> m_form;
 
-    bool m_started;
-
+    bool m_started{false};
     std::mutex m_lock;
 
-    IUnoPluginController & m_controller;
+    IUnoPluginController& m_controller;
+    std::string m_baseDir;  // Carpeta base para guardar datos
 };
